65817423de7360912be44e0edcc8379d949041b0
Jatin <jatinsharma9038@gmail.com>
2024-07-13 17:18:06 +0530

    Hitesh bootcamp & CPP STL complete

---
+++ charpointer.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // char *arg = new char(6);
+    // gets(arg);
+    // cout << arg << '\n';
+    // delete arg;
+
+    string s;
+    getline(cin, s);
+    cout << s << "\n";
+    string p;
+    cin >> p;
+    cout << p << "\n";
+    return 0;
+}
